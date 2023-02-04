7606dc6391577e0e1cbf62dc0841548dc59ea423
Elizaveta178 <elizaveta9719@gmail.com>
2023-02-04 18:27:50 +0500

    Fixing bugs

--- Assets/Scripts/Player/ShooterScripts/ShootingScript.cs
+++ Assets/Scripts/Player/ShooterScripts/ShootingScript.cs
@@ -6,34 +6,39 @@ using UnityEngine.InputSystem;
 public class ShootingScript : MonoBehaviour
 {
     
-}
+    private Vector3 target;
+    public Camera cam;
+
+    public GameObject bullet;
+
+    private float bulletForce = 25f;
 
 
-/*private Vector3 target;
-public Camera cam;
-
-public GameObject bullet;
-
-private float bulletForce = 25f;
-
-
-private void Update()
-{
-    Vector3 difference = target - transform.rotation.eulerAngles;
-    float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-    transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-}
-
-public void setMousePos(InputAction.CallbackContext context)
-{
-    target = cam.ScreenToWorldPoint(context.ReadValue<Vector2>());
-}
-
-public void shoot(InputAction.CallbackContext context)
-{
-    if (context.performed)
+    private void Update()
     {
-        GameObject projectile = Instantiate(bullet, gameObject.transform.position, transform.rotation);
-        projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
+        // Vector3 difference = target - transform.rotation.eulerAngles;
+        // float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        // transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        Vector3 difference = target - transform.position;
+        Quaternion rotation = Quaternion.FromToRotation(transform.up, difference);
+        transform.rotation = rotation * transform.rotation;
     }
-}*/
+
+    public void setMousePos(InputAction.CallbackContext context)
+    {
+        target = cam.ScreenToWorldPoint(context.ReadValue<Vector2>());
+        target.z = 0.0f;
+    }
+
+    public void shoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameObject projectile = Instantiate(bullet, gameObject.transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+}
+
+
